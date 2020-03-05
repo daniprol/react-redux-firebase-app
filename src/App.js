5bdc8f1a84ae300279f9fdd507671a9d120a8c2f
daniprol <daniprol@hotmail.com>
2020-03-05 23:59:22 +0100

    Dashboard created

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/NavBar';
+import Dashboard from './components/dashboard/Dashboard';
 
 function App() {
   return (
     <BrowserRouter>
     <div className="App">
       <Navbar />
+      <Switch> 
+        <Route path='/' component={Dashboard} />
+      </Switch>
     </div>
     </BrowserRouter>
   );
 }
-
+ 
+// Switch makes sure that only one route is loaded at a time
 export default App;
