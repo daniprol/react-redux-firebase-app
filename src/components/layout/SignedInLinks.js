76c604a4fee1ece44165e0828001e77d7948c484
daniprol <daniprol@hotmail.com>
2020-03-06 18:13:56 +0100

    Create project form added

--- src/components/layout/SignedInLinks.js
+++ src/components/layout/SignedInLinks.js
@@ -9,7 +9,7 @@ const SignedInLinks = () => {
         <ul className="right">
             <li><NavLink to='/'>New Project</NavLink></li>
             <li><NavLink to='/'>Log out</NavLink></li>
-            <li><NavLink to='/' className='btn btn-floating pink lighten-1'>DP</NavLink></li>
+            <li><NavLink to='/' className='btn btn-floating pink lighten-1 waves-effect waves-light'>DP</NavLink></li>
         </ul>
         )
 }
