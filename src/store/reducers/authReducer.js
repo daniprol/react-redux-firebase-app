23ade2f17a3c287bd83cc9956442ae138327a88a
daniprol <daniprol@hotmail.com>
2020-03-07 13:45:40 +0100

    Store and rootReducer created

---
+++ src/store/reducers/authReducer.js
@@ -0,0 +1,8 @@
+const initState = {}
+
+// when we don't have a state it will take 'initState' as the initial state!
+const authReducer = (state = initState, action) => {
+    return state
+};
+
+export default authReducer;
---
+++ src/store/reducers/projectReducer.js
@@ -0,0 +1,8 @@
+const initState = {}
+
+// when we don't have a state it will take 'initState' as the initial state!
+const projectReducer = (state = initState, action) => {
+    return state
+};
+
+export default projectReducer;
